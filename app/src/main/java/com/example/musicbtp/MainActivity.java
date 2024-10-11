68ca8656ec05a7cd2498f5c9b272219d944f364f
Admin <lamphongnet1@gmail.com>
2024-10-12 00:00:08 +0700

    Add Home UI to fragment

--- app/src/main/java/com/example/musicbtp/MainActivity.java
+++ app/src/main/java/com/example/musicbtp/MainActivity.java
@@ -1,24 +1,70 @@
 package com.example.musicbtp;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.musicbtp.fragment.ViewPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ViewPager viewpager;
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        viewpager = findViewById(R.id.viewPager);
+        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewpager.setAdapter(adapter);
+        viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            switch (position){
+                case 0:
+                    bottomNavigationView.getMenu().findItem(R.id.menu_home).setChecked(true);
+                    break;
+                case 1:
+                    bottomNavigationView.getMenu().findItem(R.id.menu_trend).setChecked(true);
+                    break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
         });
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.menu_home) {
+                    viewpager.setCurrentItem(0);
+                } else if (id == R.id.menu_trend) {
+                    viewpager.setCurrentItem(1);
+                }
+                return true;
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/example/musicbtp/fragment/ViewPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.example.musicbtp.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new TrendFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
