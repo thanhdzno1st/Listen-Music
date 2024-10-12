6a0791b229a6ee28c1e8e8c2f11e00f68e7b6a63
Le Dai Thanh <thanh261220@gmail.com>
2024-10-12 08:43:33 +0700

    done music player

--- app/src/main/java/com/example/listenmusic/Music_Activity.java
+++ app/src/main/java/com/example/listenmusic/Music_Activity.java
@@ -26,6 +26,7 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -38,18 +39,34 @@ import com.google.android.material.tabs.TabLayoutMediator;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Music_Activity extends AppCompatActivity {
-    private ViewPager2 viewPager2Music;
+    private ViewPager2 viewPager2Music,viewPager2dsphat;
     private TabLayout mTablayout;
     private CircleImageView circle_img;
-    private ImageView bt_pause,btn_addmusic,btn_dow,bt_random;
+    private ImageView bt_pause,btn_addmusic,btn_dow,bt_random,bt_danhsach;
     private View indicator;
     private TextView tabTitle,durationPlayed,durationTotal;
     private View indicator1,indicator2,indicator3;
     private SeekBar seekbar;
     private Button btn_Kbps;
     private ConstraintLayout constraint_control;
-    private RelativeLayout mainlayout;
-    private int viewPagerHeight = 0;
+    private RelativeLayout mainlayout ,layoutMenu;
+    private int viewPagerMusicHeight = 0;
+    private int viewPagerdsphatHeight = 0;
+    private boolean isDanhSachVisible = false;
+    private int currentPosition=1;
+    private Integer fixSeekbar = null; // Khai báo biến để lưu giá trị đầu tiên
+
+
+    public Integer getFixSeekbar() {
+        return fixSeekbar;
+    }
+
+    public void setFixSeekbar(Integer fixSeekbar) {
+        if (this.fixSeekbar == null) { // Chỉ gán giá trị nếu firstValue chưa được gán
+            this.fixSeekbar = fixSeekbar;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,6 +93,9 @@ public class Music_Activity extends AppCompatActivity {
                 (tab, position) -> tab.setText(viewPagerAdapterMusic.getPageTitle(position))
         ).attach();
 
+        ViewPageAdapter_dsphat viewPageAdapterDsphat= new ViewPageAdapter_dsphat(this);
+        viewPager2dsphat.setAdapter(viewPageAdapterDsphat);
+
         Animation animAlpha = AnimationUtils.loadAnimation(this,R.anim.anim_alpha);
 
         mTablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -93,58 +113,76 @@ public class Music_Activity extends AppCompatActivity {
                 // Không cần làm gì khi tab được chọn lại
             }
         });
-        ViewTreeObserver viewTreeObserver = viewPager2Music.getViewTreeObserver();
-        viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+// Biến để lưu trạng thái
+
+        bt_danhsach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Lấy layout params hiện tại của layout
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) constraint_control.getLayoutParams();
+
+                if (isDanhSachVisible) {
+                    // Trả về trạng thái mặc định (ẩn danh sách phát)
+                    params.addRule(RelativeLayout.BELOW, R.id.view_pager2_music);
+                    constraint_control.setLayoutParams(params);
+                    viewPager2dsphat.setVisibility(View.INVISIBLE);
+                    viewPager2Music.setVisibility(View.VISIBLE);
+                    layoutMenu.setVisibility(View.VISIBLE);
+                    updateViewPager2(currentPosition);
+                    updateSeekbar(currentPosition);
+                    fadeInViews(durationPlayed, durationTotal, btn_addmusic, btn_Kbps, btn_dow);  // Fade in các view
+                    isDanhSachVisible = false;
+                } else {
+                    // Hiện danh sách phát
+                    params.addRule(RelativeLayout.BELOW, R.id.view_pager2_dsphat);
+                    constraint_control.setLayoutParams(params);
+                    viewPager2dsphat.setVisibility(View.VISIBLE);
+                    viewPager2Music.setVisibility(View.INVISIBLE);
+                    layoutMenu.setVisibility(View.INVISIBLE);
+                    updateViewPager2(3);  // Chuyển đến trang 3
+                    updateSeekbar(2);  // Cập nhật seekbar
+                    fadeOutViews(durationPlayed, durationTotal, btn_addmusic, btn_Kbps, btn_dow);  // Fade out các view
+                    isDanhSachVisible = true;
+                }
+            }
+        });
+
+
+        ViewTreeObserver viewTreeObserverMusic = viewPager2Music.getViewTreeObserver();
+        viewTreeObserverMusic.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
                 // Chỉ cần lấy chiều cao của ViewPager2 sau khi layout hoàn tất
-                viewPagerHeight = viewPager2Music.getHeight();
-                Log.d("gg", "chiều cao viewPager2Music fix cứng: " + viewPagerHeight + " pixels");
+                viewPagerMusicHeight = viewPager2Music.getHeight();
+                Log.d("gg", "chiều cao viewPager2Music fix cứng: " + viewPagerMusicHeight + " pixels");
                 // Loại bỏ listener để không bị gọi lại nhiều lần
                 viewPager2Music.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+        ViewTreeObserver viewTreeObserverds = viewPager2Music.getViewTreeObserver();
+        viewTreeObserverds.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                viewPagerdsphatHeight = viewPager2dsphat.getHeight();
+                Log.d("gg", "chiều cao viewPager2dsphat fix cứng: " + viewPagerdsphatHeight + " pixels");
+                // Loại bỏ listener để không bị gọi lại nhiều lần
+                viewPager2dsphat.getViewTreeObserver().removeOnGlobalLayoutListener(this);
             }
         });
         viewPager2Music.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
+                currentPosition=position;
+
+
                 // Cập nhật chỉ báo và SeekBar khi thay đổi trang
                 updateIndicators(position);
                 updateSeekbar(position);
+                updateViewPager2(position);
 
-                // Lấy chiều cao hiện tại của ViewPager2
-                int currentViewPagerHeight = viewPager2Music.getHeight();
-                Log.d("Distance", "Chiều cao của viewPager2Music: " + currentViewPagerHeight + " pixels");
-
-                // Tính toán các vị trí và kích thước của các thành phần
-                int[] locationOnScreen = new int[2];
-                bt_random.getLocationOnScreen(locationOnScreen);
-                int btnMiddle = locationOnScreen[1] + (bt_random.getHeight() / 2);
-                int screenHeight = getResources().getDisplayMetrics().heightPixels;
-                int distanceToBottomBtn = screenHeight - btnMiddle;
-                Log.d("Distance", "Khoảng cách từ giữa btn_random đến đáy màn hình: " + distanceToBottomBtn + " pixels");
-
-                // Tính khoảng cách từ giữa SeekBar đến đáy màn hình
-                int[] seekBarLocation = new int[2];
-                seekbar.getLocationOnScreen(seekBarLocation);
-                int seekBarMiddle = seekBarLocation[1] + (seekbar.getHeight() / 2);
-                int distanceToBottomSeekBar = screenHeight - seekBarMiddle;
-                Log.d("Distance", "Khoảng cách từ giữa SeekBar đến đáy màn hình: " + distanceToBottomSeekBar + " pixels");
-
-                // Lấy chiều cao của ConstraintLayout bao bọc
-                Log.d("Distance", "Chiều cao của constraint: " + constraint_control.getHeight() + " pixels");
-
-                // Tính toán chiều cao mới của ViewPager2 khi chuyển đổi các trang
-                if (position == 0 || position == 2) {
-                    int newHeight = currentViewPagerHeight + distanceToBottomSeekBar - distanceToBottomBtn;
-                    animateViewPagerHeightChange(newHeight);  // Animation khi tăng chiều cao
-                    fadeOutViews(durationPlayed, durationTotal, btn_addmusic, btn_Kbps, btn_dow);  // Fade out các view
-                } else if (position == 1) {
-                    Log.d("Distance", "Trang 1");
-                    animateViewPagerHeightChange(viewPagerHeight);  // Animation quay về chiều cao ban đầu
-                    fadeInViews(durationPlayed, durationTotal, btn_addmusic, btn_Kbps, btn_dow);  // Fade in các view
-                }
             }
         });
+
     }
 
     private void AnhXa() {
@@ -152,6 +190,7 @@ public class Music_Activity extends AppCompatActivity {
 //        bt_pause = findViewById(R.id.btn_pause);
         mTablayout = findViewById(R.id.tab_layout_music);
         viewPager2Music= findViewById(R.id.view_pager2_music);
+        viewPager2dsphat=findViewById(R.id.view_pager2_dsphat);
         tabTitle = findViewById(R.id.tab_title);
         indicator1 = findViewById(R.id.indicator_1);
         indicator2 = findViewById(R.id.indicator_2);
@@ -165,18 +204,10 @@ public class Music_Activity extends AppCompatActivity {
         bt_random=findViewById(R.id.btn_random);
         constraint_control=findViewById(R.id.constraint_control);
         mainlayout=findViewById(R.id.main);
+        bt_danhsach=findViewById(R.id.btn_danhsachphat);
+        layoutMenu = findViewById(R.id.layout_menu);
+
     }
-//    private void startAnimation(){
-//        Runnable runnable = new Runnable() {
-//            @Override
-//            public void run() {
-//                circle_img.animate().rotationBy(360).withEndAction(this).setDuration(10000)
-//                        .setInterpolator(new LinearInterpolator()).start();
-//            }
-//        };
-//        circle_img.animate().rotationBy(360).withEndAction(runnable).setDuration(10000)
-//                .setInterpolator(new LinearInterpolator()).start();
-//    }
     // Hàm cập nhật các chỉ báo
     private void updateIndicators(int position) {
         // Reset về trạng thái ban đầu (xám và ngắn)
@@ -233,7 +264,7 @@ public class Music_Activity extends AppCompatActivity {
         int targetWidth; // Chiều dài mới của SeekBar
 
         // Xác định chiều dài mới của SeekBar dựa vào vị trí trang
-        if (position == 0 || position == 2) {
+        if (position == 0 || position == 2 || position ==3) {
             targetWidth = 660; // Chiều dài của SeekBar ở trang 0 hoặc 2
         } else if (position == 1) {
             targetWidth = 960; // Chiều dài của SeekBar ở trang 1
@@ -270,6 +301,19 @@ public class Music_Activity extends AppCompatActivity {
             fadeOut.start(); // Bắt đầu animation
         }
     }
+    private void fadeOutViews2(View... views) {
+        for (View view : views) {
+            ObjectAnimator fadeOut = ObjectAnimator.ofFloat(view, "alpha", 1f, 0f);
+            fadeOut.setDuration(1); // Thời gian animation (300ms)
+            fadeOut.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    view.setVisibility(View.GONE); // Ẩn view sau khi fade out
+                }
+            });
+            fadeOut.start(); // Bắt đầu animation
+        }
+    }
     private void fadeInViews(View... views) {
         for (View view : views) {
             view.setVisibility(View.VISIBLE); // Hiện view trước khi bắt đầu animation
@@ -278,22 +322,72 @@ public class Music_Activity extends AppCompatActivity {
             fadeIn.start(); // Bắt đầu animation
         }
     }
+    private void updateViewPager2(int position){
+        // Lấy chiều cao hiện tại của ViewPager2
+        int currentViewPagerHeight = viewPager2Music.getHeight();
+
+
+        // Tính toán các vị trí và kích thước của các thành phần
+        int[] locationOnScreen = new int[2];
+        bt_random.getLocationOnScreen(locationOnScreen);
+        int btnMiddle = locationOnScreen[1] + (bt_random.getHeight() / 2);
+        int screenHeight = getResources().getDisplayMetrics().heightPixels;
+        int distanceToBottomBtn = screenHeight - btnMiddle;
+        Log.d("Distance", "Khoảng cách từ giữa btn_random đến đáy màn hình: " + distanceToBottomBtn + " pixels");
+
+        // Tính khoảng cách từ giữa SeekBar đến đáy màn hình
+        int[] seekBarLocation = new int[2];
+        seekbar.getLocationOnScreen(seekBarLocation);
+        int seekBarMiddle = seekBarLocation[1] + (seekbar.getHeight() / 2);
+        int distanceToBottomSeekBar = screenHeight - seekBarMiddle;
+        setFixSeekbar(distanceToBottomSeekBar);
+        Log.d("Distance", "Khoảng cách từ giữa SeekBar đến đáy màn hình: " + distanceToBottomSeekBar + " pixels");
+
+        // Lấy chiều cao của ConstraintLayout bao bọc
+        Log.d("Distance", "Chiều cao của constraint: " + constraint_control.getHeight() + " pixels");
+        int layoutmenu = layoutMenu.getHeight();
+        // Tính toán chiều cao mới của ViewPager2 khi chuyển đổi các trang
+        if (position == 0 || position == 2) {
+            int newHeight = viewPagerMusicHeight + getFixSeekbar() - distanceToBottomBtn;
+            int newHeight2= viewPagerdsphatHeight + getFixSeekbar() - distanceToBottomBtn;
+            animateViewPagerHeightChange(viewPager2Music,newHeight);
+            animateViewPagerHeightChange(viewPager2dsphat,newHeight2);
+            if (viewPager2Music.getHeight() != newHeight)
+                // Fade out the specified views
+                fadeOutViews(durationPlayed, durationTotal, btn_addmusic, btn_Kbps, btn_dow);
+            else
+                fadeOutViews2(durationPlayed, durationTotal, btn_addmusic, btn_Kbps, btn_dow);
+
+        } else if (position == 1) {
+            Log.d("Distance", "Trang 1");
+            animateViewPagerHeightChange(viewPager2Music,viewPagerMusicHeight);
+            animateViewPagerHeightChange(viewPager2dsphat,viewPagerdsphatHeight);
+            fadeInViews(durationPlayed, durationTotal, btn_addmusic, btn_Kbps, btn_dow);  // Fade in các view
+        } else {
+            Log.d("Distance", "Toi o day");
+            int newHeight = viewPagerMusicHeight + getFixSeekbar() - distanceToBottomBtn;
+            int newHeight2= viewPagerdsphatHeight + getFixSeekbar() - distanceToBottomBtn;
+            animateViewPagerHeightChange(viewPager2Music,newHeight);
+            animateViewPagerHeightChange(viewPager2dsphat,newHeight2);
+            fadeOutViews(durationPlayed, durationTotal, btn_addmusic, btn_Kbps, btn_dow);  // Fade out các view
+        }
+        Log.d("Distance", "Chiều cao của viewPagerMusic : " + viewPager2Music.getHeight() + " pixels");
+        Log.d("Distance", "Chiều cao của viewPagerdsphat : " + viewPager2dsphat.getHeight() + " pixels");
+    }
 
     /**
      * Hàm để thực hiện animation thay đổi chiều cao của ViewPager2
      */
-    private void animateViewPagerHeightChange(int newHeight) {
-        ValueAnimator animator = ValueAnimator.ofInt(viewPager2Music.getHeight(), newHeight);
+    private void animateViewPagerHeightChange(ViewPager2 viewPager, int newHeight) {
+        ValueAnimator animator = ValueAnimator.ofInt(viewPager.getHeight(), newHeight);
         animator.setDuration(300); // 0.3 giây
         animator.addUpdateListener(animation -> {
-            ViewGroup.LayoutParams params = viewPager2Music.getLayoutParams();
+            ViewGroup.LayoutParams params = viewPager.getLayoutParams();
             params.height = (int) animation.getAnimatedValue();
-            viewPager2Music.setLayoutParams(params);
+            viewPager.setLayoutParams(params);
         });
         animator.start();
     }
-
-
 
 
 
