b3f1d3be2ed90b77df320e149967a6e107e9d60c
Le Dai Thanh <thanh261220@gmail.com>
2024-12-06 22:09:04 +0700

    Done

---
+++ app/src/main/java/com/example/listenmusic/fragment/ThongtinSvThuchienFragment.java
@@ -0,0 +1,81 @@
+package com.example.listenmusic.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.listenmusic.MainActivity;
+import com.example.listenmusic.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ThongtinSvThuchienFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ThongtinSvThuchienFragment extends Fragment {
+    private ImageView buttonBack;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ThongtinSvThuchienFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ThongtinSvThuchienFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ThongtinSvThuchienFragment newInstance(String param1, String param2) {
+        ThongtinSvThuchienFragment fragment = new ThongtinSvThuchienFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_thongtin_sv_thuchien, container, false);
+        buttonBack = view.findViewById(R.id.bt_back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Quay lại fragment trước đó
+                // Gọi phương thức để đóng fragment
+                if (getActivity() instanceof MainActivity) {
+                    ((MainActivity) getActivity()).closeCurrentFragment();
+                }
+            }
+        });
+        return view;
+    }
+}
